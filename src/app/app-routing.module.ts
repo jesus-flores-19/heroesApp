9a6028a6d8b23da5a81e627eb70420bfbf1d4bc3
Jesus Arath <jesus.flores.p19@gmail.com>
2021-10-21 23:10:00 -0500

    Creamos rutas globales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 
 const routes: Routes = [
-  {}
+  {path: "heroe", component: HeroeComponent},
+  {path: "heroes", component: HeroesComponent},
+  {path: "**", pathMatch: "full", redirectTo: "heroes"}
 ];
 
 @NgModule({
