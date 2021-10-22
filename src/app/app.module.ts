00e7e5e4073b79d9d7cefc092efc6b195a4600d7
Jesus Arath <jesus.flores.p19@gmail.com>
2021-10-21 23:07:36 -0500

    Creamos componente padre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
